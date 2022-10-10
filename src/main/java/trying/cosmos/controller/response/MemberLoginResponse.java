29761026137c206f2994a4b0f17fe448b658bf07
Yeongwoo-Poding <yw1221@g.skku.edu>
2022-10-10 17:03:52 +0900

    [Feature] MemberController 생성

---
+++ src/main/java/trying/cosmos/controller/response/MemberLoginResponse.java
@@ -0,0 +1,14 @@
+package trying.cosmos.controller.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberLoginResponse {
+
+    private String access_token;
+}
