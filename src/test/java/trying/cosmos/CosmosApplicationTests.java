7f30a1217a0cc06a7be8a0f7178bb70c34aa91bb
Yeongwoo-Poding <yw1221@g.skku.edu>
2022-10-10 14:32:04 +0900

    [Docs] 프로젝트 설정

---
+++ src/test/java/trying/cosmos/CosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package trying.cosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
