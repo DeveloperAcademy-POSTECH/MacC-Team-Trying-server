c9ba594781f19a7f0b033277dda0ffe0038415e6
Yeongwoo-Poding <yw1221@g.skku.edu>
2022-10-25 13:12:39 +0900

    [Refactor] #59 : 요구사항 반영

--- src/main/java/trying/cosmos/domain/planet/dto/response/PlanetCourseContent.java
+++ src/main/java/trying/cosmos/domain/planet/dto/response/PlanetCourseContent.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import trying.cosmos.domain.course.entity.Course;
 import trying.cosmos.domain.course.entity.Star;
 import trying.cosmos.domain.course.service.StarSignGenerator;
+import trying.cosmos.global.utils.date.DateUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,11 +18,13 @@ import java.util.stream.Collectors;
 public class PlanetCourseContent {
 
     private Long courseId;
+    private String createdDate;
     private String title;
     private List<Star> stars;
 
     public PlanetCourseContent(Course course) {
         this.courseId = course.getId();
+        this.createdDate = DateUtils.getFormattedDate(course.getCreatedDate());
         this.title = course.getTitle();
         this.stars = StarSignGenerator.generate(course.getTags().stream()
                 .map(tag -> tag.getPlace().getCoordinate())
--- src/main/java/trying/cosmos/domain/planet/dto/response/PlanetFindContent.java
+++ src/main/java/trying/cosmos/domain/planet/dto/response/PlanetFindContent.java
@@ -10,11 +10,13 @@ import trying.cosmos.domain.planet.entity.PlanetImageType;
 @AllArgsConstructor
 public class PlanetFindContent {
 
+    private Long planetId;
     private String name;
     private int dday;
     private PlanetImageType image;
 
     public PlanetFindContent(Planet planet) {
+        this.planetId = planet.getId();
         this.name = planet.getName();
         this.dday = planet.getDday();
         this.image = planet.getImage();
